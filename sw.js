1f19144fc1ed9922bb021404f5da12d39c527df9
Lovrodujmic1 <58843880+Lovrodujmic1@users.noreply.github.com>
2023-04-17 22:59:23 +0200

    test

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
 let currentPath = self.location.href.replace("/sw.js", "");
 
+const cacheName = "cards-workout-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/script.js"
+ 
+].map(url => currentPath + url)
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('my-pwa-cache').then(function(cache) {
